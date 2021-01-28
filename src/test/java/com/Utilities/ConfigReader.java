04be4e5713cb90acd730724fee8f60e2c5f765d5
Egemen Eliz <egemenelz@gmail.com>
2021-01-28 15:35:41 -0500

    TODO List

---
+++ src/test/java/com/Utilities/ConfigReader.java
@@ -0,0 +1,24 @@
+package com.Utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static final Properties properties;
+
+    static {
+        properties = new Properties();
+
+        try{
+            FileInputStream file = new FileInputStream("config.properties");
+            properties.load(file);
+            file.close();
+        }catch (IOException e){
+
+        }
+    }
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
---
+++ src/test/java/com/Utilities/Data.java
@@ -0,0 +1,5 @@
+package com.Utilities;
+
+public class Data {
+    public final static String user1 = "";
+}

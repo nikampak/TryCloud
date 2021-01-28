477a89db612e23812ace1bdd2a318a3014479928
Egemen Eliz <egemenelz@gmail.com>
2021-01-28 16:10:43 -0500

    New Changes

---
+++ src/test/java/com/TryCloud/Tests/Login/LogIn.java
@@ -0,0 +1,25 @@
+package com.TryCloud.Tests.Login;
+
+import com.TryCloud.Pages.LoginPage;
+import com.TryCloud.Tests.TestBase.TestBase;
+import com.Utilities.Data;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LogIn extends TestBase {
+
+    @Test
+    public void Login() {
+        LoginPage log = new LoginPage(driver);
+
+        log.username().sendKeys(Data.user1);
+        log.password().sendKeys(Data.password);
+        log.loginButton().click();
+
+        //Verify Users Land Home Page
+        String expectedTitle = "Files - Trycloud - QA";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(expectedTitle, actualTitle);
+    }
+}
--- src/test/java/com/TryCloud/Tests/TestBase/TestBase.java
+++ src/test/java/com/TryCloud/Tests/TestBase/TestBase.java
@@ -1,19 +1,27 @@
 package com.TryCloud.Tests.TestBase;
 
+import com.Utilities.WebDriverUtilities;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
     public WebDriver driver;
 
-    public TestBase(WebDriver driver) {
-        this.driver = driver;
+    public TestBase() {
+        this.driver =  WebDriverUtilities.getDriver("chrome");
     }
 
+    @BeforeClass
     public void setup(){
-
+        //driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("http://qa.trycloud.net/index.php/login?clear=1");
     }
-
+    @AfterClass
     public void tearDowns(){
-
+        driver.close();
     }
 }

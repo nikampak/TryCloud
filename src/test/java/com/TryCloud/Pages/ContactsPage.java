0bd77514a7eb1465d2facd22bb6ae1d8d5677650
Veronica Pak <nikampak@gmail.com>
2021-01-29 16:58:35 -0500

    added ContactsPage.java

---
+++ src/test/java/com/TryCloud/Pages/ContactsPage.java
@@ -0,0 +1,25 @@
+package com.TryCloud.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ContactsPage {
+    WebDriver driver;
+
+    public ContactsPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(css = "button#new-contact-button")
+    WebElement newContract;
+    @FindBy(xpath = "//span[@title='+ New group']")
+    WebElement newGroup;
+    @FindBy(xpath = "//button[@class='icon action-item__menutoggle icon-add']")
+    WebElement plusIconAction;
+    @FindBy(xpath = "//button[@class='primary']")
+    WebElement createContact;
+
+}

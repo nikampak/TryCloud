854b9a9f0c666ff24bdafe9bb35c9c3a63025b6f
Veronica Pak <nikampak@gmail.com>
2021-01-30 22:43:27 -0500

    added locators to TalkPage.java

--- src/test/java/com/TryCloud/Pages/TalkPage.java
+++ src/test/java/com/TryCloud/Pages/TalkPage.java
@@ -1,6 +1,8 @@
 package com.TryCloud.Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import sun.jvm.hotspot.debugger.Page;
 
@@ -10,5 +12,39 @@ public class TalkPage {
     public TalkPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
+    }
+    @FindBy(xpath = "//input[@placeholder='Search conversations or users']")
+    WebElement searchConversationsOrUsers;
+    @FindBy(xpath = "//button[@class='toggle has-tooltip']")
+    WebElement plusIcon;
+    @FindBy(xpath = "//div[@class='acli__content__line-one']")
+    WebElement talkUpdates;
+    @FindBy(xpath = "//button[@aria-label='Conversation settings']")
+    WebElement menuToggle;
+    @FindBy(xpath = "//a[@class='app-navigation-toggle']")
+    WebElement navigationToggle;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebElement getSearchConversationsOrUsers() {
+        return searchConversationsOrUsers;
+    }
+
+    public WebElement getPlusIcon() {
+        return plusIcon;
+    }
+
+    public WebElement getTalkUpdates() {
+        return talkUpdates;
+    }
+
+    public WebElement getMenuToggle() {
+        return menuToggle;
+    }
+
+    public WebElement getNavigationToggle() {
+        return navigationToggle;
     }
 }

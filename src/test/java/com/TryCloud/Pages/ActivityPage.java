f0bd56ea6bfe983cfe24705c9ac790ca35996142
Veronica Pak <nikampak@gmail.com>
2021-01-30 22:22:18 -0500

    added locators to ActivityPage.java

--- src/test/java/com/TryCloud/Pages/ActivityPage.java
+++ src/test/java/com/TryCloud/Pages/ActivityPage.java
@@ -1,6 +1,8 @@
 package com.TryCloud.Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ActivityPage {
@@ -8,7 +10,58 @@ public class ActivityPage {
 
     public ActivityPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(xpath = "//a[@data-navigation='all']")
+    WebElement allActivities;
+    @FindBy(xpath = "//a[@data-navigation='self']")
+    WebElement byYou;
+    @FindBy(xpath = "//a[@data-navigation='files_favorites']")
+    WebElement favorites;
+    @FindBy(xpath = "//a[@data-navigation='files']")
+    WebElement fileChanges;
+    @FindBy(xpath = "//a[@data-navigation='files_sharing']")
+    WebElement fileShares;
+    @FindBy(xpath = "//a[@data-navigation='calendar']")
+    WebElement calendar;
+    @FindBy(xpath = "//a[@data-navigation='calendar_todo']")
+    WebElement todos;
+    @FindBy(xpath = "//a[@data-navigation='comments']")
+    WebElement comments;
 
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebElement getAllActivities() {
+        return allActivities;
+    }
+
+    public WebElement getByYou() {
+        return byYou;
+    }
+
+    public WebElement getFavorites() {
+        return favorites;
+    }
+
+    public WebElement getFileChanges() {
+        return fileChanges;
+    }
+
+    public WebElement getFileShares() {
+        return fileShares;
+    }
+
+    public WebElement getCalendar() {
+        return calendar;
+    }
+
+    public WebElement getTodos() {
+        return todos;
+    }
+
+    public WebElement getComments() {
+        return comments;
     }
 }

a92c53f4aeb7bb790ba2b74a736781ef75e46942
Egemen Eliz <egemenelz@gmail.com>
2021-01-31 19:04:00 -0500

    Main Modules

--- src/test/java/com/TryCloud/Tests/MainModule/MainModules.java
+++ src/test/java/com/TryCloud/Tests/MainModule/MainModules.java
@@ -6,8 +6,6 @@ import com.TryCloud.Tests.Base.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class MainModules extends TestBase {
 
@@ -15,21 +13,19 @@ public class MainModules extends TestBase {
     public void verifyMainModules(){
         HomePage hP = new HomePage(driver);
         hP.files().click();
-        Assert.assertEquals(driver.getTitle(), "Files - Trycloud - QA");
+        Assert.assertTrue(hP.files().isDisplayed());
         hP.photos().click();
-        Assert.assertEquals(driver.getTitle(), "Photos - Trycloud - QA");
+        Assert.assertTrue(hP.photos().isDisplayed());
         hP.activity().click();
-        Assert.assertEquals(driver.getTitle(), "Activity - Trycloud - QA");
+        Assert.assertTrue(hP.activity().isDisplayed());
         hP.talk().click();
-        Assert.assertEquals(driver.getTitle(), "Talk - Trycloud - QA");
+        Assert.assertTrue(hP.talk().isDisplayed());
         hP.contacts().click();
-        Assert.assertEquals(driver.getTitle(), "Contacts - Trycloud - QA");
+        Assert.assertTrue(hP.contacts().isDisplayed());
         hP.calendar().click();
-        LocalDateTime dateTime = LocalDateTime.now();
-        DateTimeFormatter  dtf = DateTimeFormatter.ofPattern("MMMM yyyy");
-        Assert.assertEquals(driver.getTitle(), dtf.format(dateTime)+" - Calendar - Trycloud - QA");
+        Assert.assertTrue(hP.calendar().isDisplayed());
         hP.notes().click();
-        Assert.assertEquals(driver.getTitle(), "Notes - Trycloud - QA");
+        Assert.assertTrue(hP.notes().isDisplayed());
 
     }
 }
